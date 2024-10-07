83fa5e2cab47013206119d4236144c029b75fae3
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-07 22:12:54 +0100

    Create react files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css'
+
+function App(props) {
+  const currDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello Buddy!</h1>
+      <h2>The time now is {currDate.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function ref() {
+  root.render(<App/>);
+}
+
+setInterval(ref, 1000);
+
+export default App;
